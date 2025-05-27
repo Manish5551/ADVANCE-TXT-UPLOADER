3a1dcc68a0a787d64e3fced8d42f96e3ab8d85c3
Manish5551 <telegramall5551@gmail.com>
2025-05-27 21:30:56 +0530

    Update logs.py

--- logs.py
+++ logs.py
@@ -1,5 +1,5 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
+# Don't Remove Credit Tg - @Ytac2
+# Ask Doubt on telegram @Ytac2 
 
 import logging
 from logging.handlers import RotatingFileHandler
